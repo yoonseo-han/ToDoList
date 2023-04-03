6cc06d8ce1c618b671814d276bc1b9e2fdf2adf6
Yoonseo Han <yooncer00@gmail.com>
2023-04-03 18:25:15 +0800

    Updated todo list function and checkbox

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
-import TodoForm from './components/TodoForm';
-import TodoList from './components/TodoList';
+import TodoList from './TodoList';
 
 function App() {
   return (
---
+++ src/TodoList.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+
+function TodoList() {
+    const [todos, setTodos] = useState([]);
+    const [filter, setFilter] = useState('all');
+
+    useEffect(() => {
+        const storedTodos = JSON.parse(localStorage.getItem('todos'));
+        if (storedTodos) {
+            setTodos(storedTodos);
+        }
+    }, []);
+    
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos]);
+
+    function addTodo(text) {
+        const newTodo = { text, completed: false };
+        setTodos([...todos, newTodo]);
+    }
+
+    function toggleTodoCompleted(index) {
+        const newTodos = [...todos];
+        newTodos[index].completed = !newTodos[index].completed;
+        setTodos(newTodos);
+    }
+
+    function setFilterValue(newFilter) {
+        setFilter(newFilter);
+    }
+    return (
+    <div>
+        <div>
+            <button onClick={() => setFilterValue('all')}>All</button>
+            <button onClick={() => setFilterValue('active')}>Active</button>
+            <button onClick={() => setFilterValue('completed')}>Completed</button>
+        </div>
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            const newTodoText = e.target.elements.todoText.value;
+            if (newTodoText.trim()) {
+                addTodo(newTodoText);
+                e.target.elements.todoText.value = '';
+            }
+        }}>
+            <input type="text" name="todoText" placeholder="Add a new todo" />
+            <button type="submit">Add</button>
+        </form>
+        <div>
+            {todos.filter(todo => {
+                if (filter === 'all') {
+                return true;
+                } else if (filter === 'active') {
+                return !todo.completed;
+                } else if (filter === 'completed') {
+                return todo.completed;
+                }
+            }).map((todo, index) => (
+                <ul className='todo-row' key={index}>
+                    <span>{todo.text}</span>
+                    <input type="checkbox" checked={todo.completed} onChange={() => toggleTodoCompleted(index)} />
+                </ul>
+            ))}
+        </div>
+    </div>
+    );
+}
+export default TodoList;
